96e1c780dc60ba12e546ee6929314d31a042b5c8
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-05 00:04:18 +0300

    Button styled 2

--- styled-intro-inClass/src/App.js
+++ styled-intro-inClass/src/App.js
@@ -1,7 +1,9 @@
+import { Button } from "./components/Button.styled"
+
 const App = () => {
   return (
     <>
-    <h1>Styled Components</h1>
+    <Button>Click</Button>
     </>
   )
 }
--- styled-intro-inClass/src/components/Button.styled.jsx
+++ styled-intro-inClass/src/components/Button.styled.jsx
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-const Button = styled.button`
+export const Button = styled.button`
   background-color: hotpink;
   color: white;
+  cursor: pointer;
+  padding: 1rem 2rem;
+  border-radius: 0.5rem;
+  border-color: pink;
+  &:hover{
+    transform: scale(0.97);
+  }
+`;
 
-`
+
