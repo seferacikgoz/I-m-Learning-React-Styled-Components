afaca06aca15841656d118f11dcb8af3c802d33f
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-05 02:29:44 +0300

    Container

--- styled-intro-inClass/src/App.js
+++ styled-intro-inClass/src/App.js
@@ -1,13 +1,19 @@
 import { Button, TomatoButton } from "./components/Button.styled"
+import Container from "./components/Container.styled"
 
 const App = () => {
   return (
     <>
+  <Container>
     <Button primary>Save</Button>
     <Button>Clear</Button>
     <TomatoButton>Send</TomatoButton>
     <TomatoButton primary>Submit</TomatoButton>
+  </Container>
+    
 
+  
+    
     </>
   )
 }
