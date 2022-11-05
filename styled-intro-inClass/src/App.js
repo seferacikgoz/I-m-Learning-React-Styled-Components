89e202a47029ef5eb44895c6c0c89bc696f08930
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-11-05 11:58:08 +0300

    HeaderText color

--- styled-intro-inClass/src/App.js
+++ styled-intro-inClass/src/App.js
@@ -1,10 +1,12 @@
 import { Button, TomatoButton } from "./components/Button.styled"
 import Container from "./components/Container.styled"
+import HeaderText from "./components/HeaderText.styled"
 
 const App = () => {
   return (
     <>
   <Container bg="grey">
+    <HeaderText color="hotpink">STYLED COMPONENTS</HeaderText>
     <Button primary>Save</Button>
     <Button>Clear</Button>
     <TomatoButton>Send</TomatoButton>
